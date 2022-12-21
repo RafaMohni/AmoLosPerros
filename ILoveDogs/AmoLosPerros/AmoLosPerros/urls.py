686dfec3401893189216f94fe54df52034abe4bc
RafaMohni <110611366+RafaMohni@users.noreply.github.com>
2022-12-21 16:13:28 -0300

    Update urls.py

--- ILoveDogs/AmoLosPerros/AmoLosPerros/urls.py
+++ ILoveDogs/AmoLosPerros/AmoLosPerros/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('login.html/', views.login_page),
+    path('Prueba/', include(('Prueba.migrations.urls', 'Prueba')))
 ]
